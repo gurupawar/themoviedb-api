85872651beba680507f7f00ecbb2df185700e272
Guru <g.r.pawar000@gmail.com>
2021-10-01 18:47:35 +0530

    fixed small issue

--- api/tvTopRated.js
+++ api/tvTopRated.js
@@ -1,10 +1,9 @@
 const loopMovies = require("../utils/loopMovies");
 
-module.exports = async function getTvShow() {
+module.exports = async (req, res) => {
   const endPoint = "/tv";
   try {
     const tvTop = await loopMovies(endPoint);
-
     res.json(tvTop);
   } catch (error) {
     console.log(error);
