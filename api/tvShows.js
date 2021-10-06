09c087856f2057271ad1e01ca6b48535742356e1
Guru Pawar <g.r.pawar000@gmail.com>
2021-10-06 21:48:50 +0530

    header added

--- api/tvShows.js
+++ api/tvShows.js
@@ -1,6 +1,9 @@
 const loopMovies = require("../utils/loopMovies");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
   const endPoint = "/tv";
   try {
     const tvshows = await loopMovies(endPoint);
--- api/upcomingMovies.js
+++ api/upcomingMovies.js
@@ -1,6 +1,9 @@
 const loopMovies = require("../utils/loopMovies");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
   const endPoint = "/movie/upcoming";
   try {
     const ucomingMv = await loopMovies(endPoint);
